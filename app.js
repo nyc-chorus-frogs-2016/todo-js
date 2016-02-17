d978f708911add58403360acc7c1c15e3670a9c1
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-16 22:32:26 -0500

    Move models into a separate file

--- app.js
+++ app.js
@@ -1,77 +1,8 @@
 /*
-   For this morning we are not going to worry about 
+   For this morning we are not going to worry about
    namespacing or separate files or any of that shizzle.
    Normally we would of course.
 */
-function Task(args) {
-  args = args || {};
-  Object.assign(this, args);
-}
-
-Task.prototype.minutesTillDue = function() {
-  if (this.dueDate) {
-    var dueMs = this.dueDate.getTime();
-    var nowMs = new Date().getTime();
-    var diff = dueMs - nowMs;
-    return diff / 1000 / 60;
-  } else {
-    return 0;
-  }
-};
-
-Task.all = function() {
-  return $.ajax({
-    url: 'http://localhost:3000/tasks'
-  }).then(function(response) {
-    return response.map(function(item){
-      return new Task(item);
-    });
-  });
-};
-
-Task.create = function(task) {
-  return $.ajax({
-    url: 'http://localhost:3000/tasks',
-    method: 'POST',
-    data: {
-      description: task.description,
-      dueDate: task.dueDate,
-      done: task.done || false
-    },
-    dataType: 'json'
-  }).then(function(response){
-     return new Task(response);
-  });
-};
-
-Task.prototype.destroy = function() {
-  return $.ajax({
-    url: 'http://localhost:3000/tasks/' + this.id,
-    method: 'DELETE'
-  });
-};
-
-function TodoList(tasks) {
-  this.tasks = tasks || [];
-}
-
-TodoList.prototype.addTask = function(task) {
-  this.tasks.push(task);
-};
-
-TodoList.prototype.removeTask = function(task) {
-  this.tasks = this.tasks.filter(function(item) {
-    return item != task;
-  });
-};
-
-TodoList.prototype.findTaskById = function(id) {
-  var matches = this.tasks.filter(function(task){
-    return task.id == id;
-  });
-  return matches.length ? matches[0] : undefined;
-};
-
 function View() {
   this.displayElement = document.getElementById('todo-list');
   this.button = document.getElementById('save-todo');
---
+++ models.js
@@ -0,0 +1,68 @@
+function Task(args) {
+  args = args || {};
+  Object.assign(this, args);
+}
+
+Task.prototype.minutesTillDue = function() {
+  if (this.dueDate) {
+    var dueMs = this.dueDate.getTime();
+    var nowMs = new Date().getTime();
+    var diff = dueMs - nowMs;
+    return diff / 1000 / 60;
+  } else {
+    return 0;
+  }
+};
+
+Task.all = function() {
+  return $.ajax({
+    url: 'http://localhost:3000/tasks'
+  }).then(function(response) {
+    return response.map(function(item){
+      return new Task(item);
+    });
+  });
+};
+
+Task.create = function(task) {
+  return $.ajax({
+    url: 'http://localhost:3000/tasks',
+    method: 'POST',
+    data: {
+      description: task.description,
+      dueDate: task.dueDate,
+      done: task.done || false
+    },
+    dataType: 'json'
+  }).then(function(response){
+     return new Task(response);
+  });
+};
+
+Task.prototype.destroy = function() {
+  return $.ajax({
+    url: 'http://localhost:3000/tasks/' + this.id,
+    method: 'DELETE'
+  });
+};
+
+function TodoList(tasks) {
+  this.tasks = tasks || [];
+}
+
+TodoList.prototype.addTask = function(task) {
+  this.tasks.push(task);
+};
+
+TodoList.prototype.removeTask = function(task) {
+  this.tasks = this.tasks.filter(function(item) {
+    return item != task;
+  });
+};
+
+TodoList.prototype.findTaskById = function(id) {
+  var matches = this.tasks.filter(function(task){
+    return task.id == id;
+  });
+  return matches.length ? matches[0] : undefined;
+};
