e7d7f97199f054537a9ee75251ec69da38eeaf7f
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-16 22:55:57 -0500

    Add deletion

--- components.jsx
+++ components.jsx
@@ -1,37 +1,47 @@
-var Task = React.createClass({
-   render: function() {
-   	  var t = this.props.task;
-   	  return(
-   	  	<li>{t.description}
-   	  	<input type="checkbox" />
-   	  	{moment(t.dueDate).fromNow()}
-   	  	</li>
-   	  	);
-   }
+var SingleTask = React.createClass({
+ render: function() {
+   var t = this.props.task;
+   return(
+    <li>
+      {t.description}
+      <input type="checkbox" />
+      {moment(t.dueDate).fromNow()}
+      <button onClick={this.props.deleteTask}>Delete</button>
+    </li>
+    );
+ }
 });
 
 var TaskList = React.createClass({
+  loadCurrentTasks: function(){
+    Task.all().then(function(response){
+      this.setState({tasks: response});
+    }.bind(this));
+  },
+  delTask: function(id) {
+    console.log('del task called with', arguments);
+    Task.delete(id).then(function() {
+      this.loadCurrentTasks();
+    }.bind(this));
+  },
   getInitialState: function() {
-      return {
-        tasks: []  
-      };
+    return {
+      tasks: []
+    };
   },
   componentWillMount: function() {
-  	$.get('http://localhost:3000/tasks').then(function(response){
-  		this.setState({tasks: response});
-  	}.bind(this));      
+    this.loadCurrentTasks();
   },
   render: function(){
-  	var rows = [];
-  	this.state.tasks.forEach(function(task){
-  		rows.push(<Task task={task} key={task.id} />);
-  	})
-  	this.state
-  	return(
-  		<div>{rows}</div>
-  		);
+    var rows = [];
+    this.state.tasks.forEach(function(task){
+      rows.push(<SingleTask deleteTask={this.delTask.bind(this, task.id)} task={task} key={task.id} />);
+    }.bind(this));
+    this.state
+    return (
+      <div>{rows}</div>
+    );
   }
 });
-
 
 ReactDOM.render(<TaskList />, document.getElementById('todo-list'));
